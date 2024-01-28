56dc2b9802b4a90208cd8d5c8841c6819292361d
George Ho <gho@forwardfinancing.com>
2024-01-28 16:52:31 -0500

    Lint / clean up storage main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,18 +1,18 @@
 locals {
-  example_bucket_name = "${var.bucket_name}"
-  example_bucket_log_name = "${var.bucket_name}_s3_logs"
+  example_bucket_name      = var.bucket_name
+  example_bucket_log_name  = "${var.bucket_name}_s3_logs"
 }
 
+# Create an S3 bucket
 resource "aws_s3_bucket" "example" {
-  bucket = local.example_bucket_name
-
-  # In case bucket is mission critical. Do not allow Terraform to delete it if there are still files in it.
-  force_destroy = false
+  bucket       = local.example_bucket_name
+  force_destroy = false # Ensure Terraform doesn't delete the bucket if files are present
 }
 
-
+# Configure server-side encryption for the S3 bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.example.bucket
+
   rule {
     bucket_key_enabled = false
     apply_server_side_encryption_by_default {
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   }
 }
 
-# Block all public access
+# Configure public access blocking for the S3 bucket
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
@@ -31,15 +31,16 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = true
 }
 
-# Enable versioning
+# Enable versioning for the S3 bucket
 resource "aws_s3_bucket_versioning" "example" {
   bucket = aws_s3_bucket.example.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# Disable ACLs
+# Configure ownership controls for the S3 bucket
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
 
@@ -48,15 +49,16 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
+# Configure logging for the S3 bucket
 resource "aws_s3_bucket_logging" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  target_bucket = aws_s3_bucket.bucket.example.id
+  bucket       = aws_s3_bucket.example.id
+  target_bucket = aws_s3_bucket.example.id
   target_prefix = "log/"
 }
 
+# Create a CloudTrail to capture S3 bucket access events
 resource "aws_cloudtrail" "example_bucket_logs" {
-  name                          = "${local.example_bucket_log_name}"
+  name                          = local.example_bucket_log_name
   s3_bucket_name                = aws_s3_bucket.example.id
   enable_logging                = true
   include_global_service_events = true
