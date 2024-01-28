f58c1e6a3fd37cf3baa2e49d692620cf2c5f0242
George Ho <gho@forwardfinancing.com>
2024-01-28 16:57:57 -0500

    Update security group

--- modules/core/asg.tf
+++ modules/core/asg.tf
@@ -81,18 +81,34 @@ resource "aws_cloudwatch_metric_alarm" "example_cpu_alarm_down" {
 
 # move this later
 resource "aws_security_group" "example" {
-  name = "example_http"
+  name = local.app
+
   vpc_id = aws_vpc.example.id # pretend this exists
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    description      = "Allow inbound HTTPS traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Allow inbound HTTP traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port = 80
-    to_port = 80
-    protocol = "-1"
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
