06594151b925b311374a6f35d03a24f8cff1cc6e
George Ho <gho@forwardfinancing.com>
2024-01-28 17:14:12 -0500

    Move security group to separate file

--- modules/core/asg.tf
+++ modules/core/asg.tf
@@ -78,37 +78,3 @@ resource "aws_cloudwatch_metric_alarm" "example_cpu_alarm_down" {
     AutoScaleGroupName = aws_autoscaling_group.example.name
   }
 }
-
-# move this later
-resource "aws_security_group" "example" {
-  name = local.app
-
-  vpc_id = aws_vpc.example.id # pretend this exists
-
-  ingress {
-    description      = "Allow inbound HTTPS traffic"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "Allow inbound HTTP traffic"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    description      = "Allow all outbound traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
